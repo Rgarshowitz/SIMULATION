e69bb1665e8f9cd01758e9a6e82ec4f3671ac074
Ron Garshowitz <49078523+Rgarshowitz@users.noreply.github.com>
2021-11-27 00:02:23 +0200

    Update main.py
    Added to patch 2:
    loc_size_distributions : list of the lambda distributions per location
    package arrival execution function

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class Package():
     def __init__(self, size, first_sending_option, destination, cur_location=None, bin_size=None, 
     is_priority=False, ft_sent=None, st_sent=None, days_in_center=0):
         self.size=size
-        self.size=size
         self.first_sending_option=first_sending_option
         self.destination=destination
         self.cur_location=cur_location
@@ -28,6 +27,9 @@ class Package():
         self.st_sent=st_sent
         self.days_in_center=days_in_center
 
+    def __repr__(self):
+        return f'size: {self.size} destination:{self.destination} first_sending_option:{self.first_sending_option}'
+    
     def __lt__(self, package2):
         return self.first_sending_option < package2.first_sending_option
     
@@ -62,13 +64,13 @@ Dest6.neighbors["big"], Dest6.neighbors["medium"], Dest6.neighbors["small"]=[5,4
 
 # create 18 package heaps in two dictionareis Vip and regular : the dict key is the heap name (i,j) value heap the 
 vip_heap_dict = {}
-for i in range(6):
-    for j in range(3):
+for i in range(1,7):
+    for j in range(1,4):
         vip_heap_dict[i, j] = []
 regular_heap_dict = {}
-for i in range(6):
-    for j in range(3):
-        vip_heap_dict[i, j] = []
+for i in range(1,7):
+    for j in range(1,4):
+        regular_heap_dict[i, j] = []
 
 P=[]   #Event heap
 NOW=0   #Current simulation time
@@ -78,4 +80,33 @@ returned_num=0   #Number of packages that has been returned to the logistics cen
 returned_clients_num=0   # number of clients that had to come back later because of destination malfunction
 packages_in_center={"big":{}, "medium":{}, "small":{}}   #Dictionary that counts how many packages are left each days in center by category
 days_to_delivery={1:{}, 2:{}, 3:{}, 4:{}, 5:{}, 6:{}}   # Counts how much time passed for each package in center for each destination, by days
+loc_size_distributions = {(1,1):1,  (1,2):3, (1,3):7,
+                          (2,1):1.5,(2,2):2, (2,3):8,
+                          (3,1):2,  (3,2):4, (3,3):12,
+                          (4,1):3,  (4,2):1, (4,3):5,
+                          (5,1):1,  (5,2):3, (5,3):8,
+                          (6,1):1.5,(6,2):1, (6,3):3 } 
+
+#### Funcitons ####
+def add_x_packages_to_heap(NOW,i,j,x): #adds x packages to heap (i,j)
+    t = 0
+    while t<x:
+        Package(j,NOW,i).push_to_heap()
+        t+=1
+    return None
+
+def package_arrival_execution(NOW): #Create daily packages
+    for i in range(1,7):
+        for j in range(1,4):
+            x = np.random.poisson(loc_size_distributions[(i,j)])
+            #print(f'destination:{i},size:{j} x:{x}')
+            if F ==0:
+                add_x_packages_to_heap(NOW,i,j,x)
+            else:
+                if NOW%7 == 0:
+                    add_x_packages_to_heap(NOW+1,i,j,x)
+                else:
+                    add_x_packages_to_heap(NOW,i,j,x)
+    return None ### ADD package send creation
+    
 
